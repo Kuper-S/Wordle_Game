22b10084af880c5309ee7e32fb32fc35fb0eefd7
Kuper-S <kupkup1992@gmail.com>
2023-06-18 11:54:06 +0300

    remove keys

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,11 +1,14 @@
 import "../../App.css";
+import { ToastContainer, toast } from 'react-toastify';
+import ReactModal from 'react-modal';
+import { FaQuestionCircle, FaTimes } from 'react-icons/fa';
 import Board from "./Board";
 import Keyboard from "./Keyboard";
 import { boardDefault, generateWordSet } from "./Words";
 import React, { useState, useEffect,createContext } from "react";
 import GameOver from "./GameOver";
 
-
+ReactModal.setAppElement('#root');
 export const AppContext = createContext();
 
 function GamePage() {
@@ -14,6 +17,7 @@ function GamePage() {
   const [wordSet, setWordSet] = useState(new Set());
   const [correctWord, setCorrectWord] = useState("");
   const [disabledLetters, setDisabledLetters] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [gameOver, setGameOver] = useState({
     gameOver: false,
     guessedWord: false,
@@ -34,7 +38,7 @@ function GamePage() {
     if (wordSet.has(enteredWord.toLowerCase())) {
       setCurrAttempt({ attempt: currAttempt.attempt + 1, letter: 0 });
     } else {
-      alert("Word not found");
+      toast.error("Word not found"); // Display error message using toast.error
     }
   
     if (enteredWord.toLowerCase() === correctWord.toLowerCase()) {
@@ -66,12 +70,45 @@ function GamePage() {
       letter: currAttempt.letter + 1,
     });
   };
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
 
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
   return (
     <div className="gamepage">
       <nav>
         <h1>Wordle</h1>
+        <div className="tooltip-container">
+          <FaQuestionCircle onClick={openModal} className="question-icon" />
+        </div>
       </nav>
+
+      <ReactModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <div className="modal-content">
+          <h2>How To Play</h2>
+          <p>Guess the Wordle in 6 tries.</p>
+          <p>Each guess must be a valid 5-letter word.</p>
+          <p>The color of the tiles will change to show how close your guess was to the word.</p>
+          <h3>Examples</h3>
+          <ul>
+            <li>W is in the word and in the correct spot.</li>
+            <li>I is in the word but in the wrong spot.</li>
+            <li>U is not in the word in any spot.</li>
+          </ul>
+          <p>A new puzzle is released daily at midnight. If you haven’t already, you can sign up for our daily reminder email.</p>
+          <p>Have feedback? Email us at xyz@xyz.com.</p>
+        </div>
+        <FaTimes onClick={closeModal} className="close-icon" />
+      </ReactModal>
+      <ToastContainer />
       <AppContext.Provider
         value={{
           board,
