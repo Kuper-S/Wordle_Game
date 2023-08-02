5fb421a60c4679331a3e51b5ff0369df6f79cae0
Kuper-S <kupkup1992@gmail.com>
2023-08-02 10:26:42 +0300

    add and remove some files

---
+++ public/assets/AnimatedTitle/animatedTitle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./animatedTitle.css"
+
+function AnimatedTitle() {
+  return (
+  <div className='animated-continer'>
+    <div className="center">
+        <h1>
+         <span>WORDLE</span>
+         <span>WORDLE</span>
+         <span>WORDLE</span>
+        </h1>
+    </div>
+  </div>
+  )
+};
+
+export default AnimatedTitle;
--- src/pages/GamePage/Words.js
+++ src/pages/GamePage/Words.js
@@ -1,5 +1,3 @@
-import wordBank from "../../utils/wordle-bank.txt";
-
 export const boardDefault = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -9,15 +7,3 @@ export const boardDefault = [
   ["", "", "", "", ""],
 ];
 
-export const generateWordSet = async () => {
-  let wordSet;
-  let todaysWord;
-  await fetch(wordBank)
-    .then((response) => response.text())
-    .then((result) => {
-      const wordArr = result.split("\n");
-      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)].trim();
-      wordSet = new Set(wordArr.map((word) => word.trim()));
-    });
-  return { wordSet, todaysWord };
-};
