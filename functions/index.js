58b39bd3d2d84ea43ae9839c50b6a159392c862f
Kuper-S <kupkup1992@gmail.com>
2023-08-02 08:54:00 +0300

    edit firebase functoin

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const admin = require('firebase-admin');
-const fs = require('fs');
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
